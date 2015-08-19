829f6f57cad9c62fa795a565b6dd9b311cfabd9d
cosmangabriel <cosmangabriel@gmail.com>
2015-08-19 15:04:47 +0300

    Subscribe file

---
+++ subscribe.tpl.php
@@ -0,0 +1,10 @@
+<html>
+	<head>
+		<h2> Subscribe to be notifed when stuff happens </h2>
+	</head>	
+		<body>
+			Email:<br>
+			<input type="text" name="firstname">
+			<br><br>
+			<input type="submit" value="Subscribe">
+		</body>
