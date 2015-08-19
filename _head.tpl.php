c01589d386f04cca3c966f9e034177fc748eb19c
lena <lena.ipatii@gmail.com>
2015-08-19 21:50:55 +0300

    - the files where renamed in *.tpl.php
    - created the /css/style.css and include it. The /less/style.less will be used only as a source and with it the css file will be generated
    - index.php was deleted because right now it was empty

---
+++ _head.tpl.php
@@ -0,0 +1,9 @@
+	<title>M(i)steryBook.com</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+ 	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/less.js/2.5.1/less.min.js"></script>
+
