725e6eea989a48e8cd971886d47e73478df9a23f
lena <lena.ipatii@gmail.com>
2015-08-19 21:59:53 +0300

    - created the index.tpl.php file that contains the middle content of the homepage

---
+++ index.tpl.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "_head.tpl.php"; ?>
+	</head>
+
+	<body>
+		<?php include "_header.tpl.php"; ?>
+		<div class= "content-carusel">
+			<div id="mycarousel" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="img/misteryhome.jpg" alt="" class="img-responsive">
+					   <div class="carousel-caption"> Welcome to the best collaborative writing book!</div> 
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="middle container">
+			<h2>How does it work?</h2>
+			<div class="row bs-wizard col-md-offset-1">			   
+				<div class="col-xs-2 bs-wizard-step complete">
+					<div class="text-center bs-wizard-stepnum">Step 1</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">propose ideas</div>
+				</div>
+
+				<div class="col-xs-2 bs-wizard-step complete"><!-- complete -->
+					<div class="text-center bs-wizard-stepnum">Step 2</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">vote an idea</div>
+				</div>
+
+				<div class="col-xs-2 bs-wizard-step active"><!-- complete -->
+					<div class="text-center bs-wizard-stepnum">Step 3</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">write the book</div>
+				</div>
+
+				<div class="col-xs-2 bs-wizard-step disabled"><!-- active -->
+					<div class="text-center bs-wizard-stepnum">Step 4</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">choose a title</div>
+				</div>
+				<div class="col-xs-2 bs-wizard-step disabled"><!-- active -->
+					<div class="text-center bs-wizard-stepnum">Step 5</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">enjoy the glory</div>
+				</div>
+			</div>
+			<div class = "vote">
+				<input type = "Submit" class = "btn btn-success" Name = "Submit" value = "Click here to vote!" />
+			</div>
+		</div>
+		<?php include "_footer.tpl.php"; ?>   
+	</body>
+</html>
+
+  
