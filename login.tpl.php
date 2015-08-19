4d6140a6a477fe00c17583027ec9654b5bc2e01a
andrei-acm <acm.andrei95@gmail.com>
2015-08-19 12:28:24 +0300

    login.tpl.php

---
+++ login.tpl.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+	<title>Login</title>
+	</head>
+<body> 
+	<div> 
+		<p><b>Login</b></p>
+				Mail<br><input type="text" name="user" size="40"><br> 
+					Password<br><input type="password" name="pass" size="40"><br> 
+						<input id="button" type="submit" name="submit" value="Login"> 
+			</form> 
+	</div> 
+</body>
+</html>
