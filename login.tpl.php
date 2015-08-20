67ea859379ad3f0619304135c84f3898531e4ab8
andrei-acm <acm.andrei95@gmail.com>
2015-08-20 16:28:43 +0300

    added form

--- login.tpl.php
+++ login.tpl.php
@@ -4,11 +4,15 @@
 	</head>
 <body> 
 	<div> 
-		<p><b>Login</b></p>
-				Mail<br><input type="text" name="user" size="40"><br> 
-					Password<br><input type="password" name="pass" size="40"><br> 
+		<fieldset style="width:30%"><legend><p><b>Login</b></p></legend>
+					<form method="POST" action="login.php"> 
+					<input type="hidden" name="formname" value="sign-in">
+						Mail<br><input type="text" name="user" size="40"><br> 
+						Password<br><input type="password" name="pass" size="40"><br> 
 						<input id="button" type="submit" name="submit" value="Login"> 
-			</form> 
+					</form> 
+		</fieldset> 
 	</div> 
 </body>
 </html>
+
