f468a679bd7e89adfce8dfee56319cc6ae02ed7e
cosmangabriel <cosmangabriel@gmail.com>
2015-08-22 08:39:04 +0300

    Update subscribe.tpl.php

--- subscribe.tpl.php
+++ subscribe.tpl.php
@@ -1,8 +1,9 @@
 <html>
 	<head>
-		<h2> Subscribe to be notifed when stuff happens </h2>
+		
 	</head>	
 		<body>
+			<h2> Subscribe to be notifed when stuff happens </h2>
 			Email:<br>
 			<input type="text" name="firstname">
 			<br><br>
