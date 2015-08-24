a1be6106b584433140e65e3ab6c99a49867b3930
kadarimola <kadarimola@yahoo.com>
2015-08-24 13:53:25 +0300

    MiB-020 Create function to retrieve details for book idea, created files genre.php, language.php, idea.php in mvc/model

---
+++ mvc/model/idea.php
@@ -0,0 +1,43 @@
+<?php
+
+	include('language.php');
+	include('genre.php');
+
+	function idea_get ($id)
+	{
+		global $dbc;
+		
+
+		$q = "SELECT * 
+		FROM ideas 
+		WHERE id = '$id'";
+
+		$r = mysqli_query($dbc, $q);
+		$idea_row = mysqli_fetch_assoc($r);
+
+		if (empty($idea_row)) {
+
+			return FALSE;
+
+		} else {
+
+			$language = language_get($id);
+			$genres = genre_get($id);
+
+			$idea = array(
+				'id' => $idea_row['id'],
+				'user_id' => $idea_row['user_id'],
+				'title' => $idea_row['title'],
+				'description' => $idea_row['description'],
+				'language' => $language,
+				'length' => $idea_row['length'],
+				'rating' => $idea_row['rating'],
+				'genres' => $genres
+			);
+
+			return $idea;
+		}
+		
+	}
+
+?>
