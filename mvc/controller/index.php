ee09a8565793821f8c782a544bb442f4afd8c3e0
Lodoaba Mihai <mihai@digitalrepublic.ro>
2015-09-05 11:49:15 +0300

    Cherry pick from MiB-016 branch commit

--- mvc/controller/index.php
+++ mvc/controller/index.php
@@ -1,3 +1,20 @@
 <?php
+	define('BOOK_STATUS_IDEAS','1');
+	define('BOOK_STATUS_VOTE','2');
+	define('BOOK_STATUS_WRITE','3');
+	define('BOOK_STATUS_TITLE','4');
+	define('BOOK_STATUS_FINISHED','5');
+
+	// Include models
+	include '../model/setting.php';
+
+	//retrieve the curent status
+	setting_get_value('book_status');
+
+	// Include config file
+	include '../config.php';
+
+	// Database connection
+	$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die(mysqli_error($dbc));
 
 ?>
---
+++ mvc/view/index_4.tpl.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include "_head.tpl.php"; ?>
+	</head>
+
+	<body>
+		<?php include "_header.tpl.php"; ?>
+		<?php $view = 'index_4'; ?>
+		<div class= "content-carusel">
+			<div id="mycarousel" class="carousel slide" data-ride="carousel">
+				<div class="carousel-inner">
+					<div class="item active">
+						<img src="../../img/misteryhome.jpg" alt="" class="img-responsive">
+					   	<div class="carousel-caption">
+					   		Welcome to the best collaborative writing book!
+					   		<a href="#" class="btn btn-sm btn-primary" title="Read the book">Read the book</a>
+					   		<a href="#" class="btn btn-sm btn-primary" title="View & vote proposed titles">View & vote proposed titles</a>
+					   		<a href="#" class="btn btn-lg btn-primary" title="Propose another title">Propose another title</a>
+					   	</div> 
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="middle container">
+			<h2>How does it work?</h2>
+			<div class="row bs-wizard col-md-offset-1">			   
+				<div class="col-xs-2 bs-wizard-step complete">
+					<div class="text-center bs-wizard-stepnum">Step 1</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">propose ideas</div>
+				</div>
+
+				<div class="col-xs-2 bs-wizard-step complete">
+					<div class="text-center bs-wizard-stepnum">Step 2</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">vote an idea</div>
+				</div>
+
+				<div class="col-xs-2 bs-wizard-step complete ">
+					<div class="text-center bs-wizard-stepnum">Step 3</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">write the book</div>
+				</div>
+
+				<div class="col-xs-2 bs-wizard-step active">
+					<div class="text-center bs-wizard-stepnum">Step 4</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">choose a title</div>
+				</div>
+				<div class="col-xs-2 bs-wizard-step disabled">
+					<div class="text-center bs-wizard-stepnum">Step 5</div>
+					<div class="progress"><div class="progress-bar"></div></div>
+					<a href="#" class="bs-wizard-dot"></a>
+					<div class="bs-wizard-info text-center">enjoy the glory</div>
+				</div>
+			</div>
+			<div class = "vote">
+				<input type = "Submit" class = "btn btn-success" Name = "Submit" value = "Click here to vote!" />
+			</div>
+		</div>
+		<?php include "_footer.tpl.php"; ?>   
+	</body>
+</html>
+
+  
